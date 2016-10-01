d55d7526bd6ac6e5cfbe25120609427b857dcef7
TinaHeiligers <tina.heiligers@gmail.com>
2016-10-01 10:10:05 -0700

    working

--- assignment1/mod1_soln/app.js
+++ assignment1/mod1_soln/app.js
@@ -6,33 +6,49 @@ angular.module('NameCalculator', [])
 .controller('NameCalculatorController', function ($scope) {
   $scope.name = "";
   $scope.totalValue = 0;
-  $scope.message = "message";
+  $scope.messageText = "";
+  // console.log($scope.messageText)
 
   $scope.displayNumeric = function () {
-    var totalNameValue = calculatNumericForString($scope.name);
-    $scope.totalValue = totalNameValue;
+    var totalStringValue = calculatNumericForString($scope.name);
+    $scope.totalValue = totalStringValue;
+    // console.log("above messageText call totalValue is:")
+    // console.log($scope.totalValue)
+    
+    var messageText = displayMessage($scope.totalValue)
+    $scope.messageText = messageText
   };
 
+  function displayMessage(totalStringValue) {
+    var messageText = calculatNumericForString($scope.name);
+    // console.log("in displayMessageTop")
+    if (messageText == "") {
+      $scope.messageText = "Please enter data first";
+      // console.log($scope.messageText)
+    }
+    else if (messageText <= 3) {
+      $scope.messageText = "Enjoy!";
+      // console.log($scope.messageText)
+    }
+    else {
+      $scope.messageText = "Too much!";
+    }
+    return $scope.messageText
+  };
 
   function calculatNumericForString(string) {
     var totalStringValue = 0;
-    totalStringValue = string.split(",").length;
+    // console.log("In calculatNumericForString")
+    // console.log(totalStringValue)
+    if (string != "") {
+      totalStringValue = string.split(",").length;
+    
+      // console.log("After totalStringValue count")
+      // console.log(totalStringValue)
+    }
+    
     return totalStringValue;
-  }
-
-  function messageForItemsCount() {
-    var totalItemsCount = displayNumeric($scope.totalValue);
-    var message = "";
-    if (totalItemsCount == "") {
-      $scope.message = "Please enter data first";
-    }
-    else if (totalItemsCount <= 3) {
-      $scope.message = "Enjoy!";
-    }
-    else {
-      $scope.message = "Too much!";
-    }
-    return $scope.message
+    
   }
     
 });

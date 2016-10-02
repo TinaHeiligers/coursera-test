669d279498b02a62da80d526bdb3269513423925
TinaHeiligers <tina.heiligers@gmail.com>
2016-10-02 10:06:38 -0700

    conditional borders done

--- assignment1/mod1_soln/app.js
+++ assignment1/mod1_soln/app.js
@@ -10,22 +10,37 @@
 
   function LunchCheckController($scope) {
     $scope.string = "";
+    // $scope.message = "";
+    $scope.status = "";
+    $scope.col = "";
+
     $scope.check = function () {
       var items = $scope.string.split(",")
+    
       var arr = []
       for (var i = 0; i < items.length; i++) {
         if (items[i] != " " && items[i] != "") {
           arr.push(items[i])
         }
       }
-    if (arr.length == 0)
-      $scope.message = "Please enter data first";
-    else {
-      if (arr.length < 4)
-        $scope.message = "Enjoy!";
-      else
-        $scope.message = "Too much!";
+    
+      if (arr.length == 0) {
+        $scope.message = "Please enter data first";
+        $scope.status = "has-error";
+        $scope.col = "red";
       }
-    };
+      else {
+      
+        if (arr.length < 4){
+          $scope.message = "Enjoy!";
+          $scope.status = "has-success";
+          $scope.col = "green";
+              }
+        else{
+          $scope.message = "Too much!";
+          $scope.status = "has-success";
+          $scope.col = "green";}
+        }
+      }
   }
 }());

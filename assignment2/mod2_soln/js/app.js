027b93d4c70e0933979df36505a37d3708ce04ac
TinaHeiligers <tina.heiligers@gmail.com>
2016-10-04 14:37:38 -0700

    requirements met

--- assignment2/mod2_soln/js/app.js
+++ assignment2/mod2_soln/js/app.js
@@ -1,73 +1,59 @@
-"use strict";
-
 (function () {
-
+'use strict';
 var shoppingList = [
-  {
-    name: "Onions",
-    quantity: "2"
-  },
-  {
-    name: "Garlic",
-    quantity: "1"
-  },
-  {
-    name: "Carrots",
-    quantity: "5"
-  },
-  {
-    name: "Celery Sticks",
-    quantity: "10"
-  },
-  {
-    name: "Potatoes",
-    quantity: "3"
-  }
-];
+          { name: "Onions",  quantity: "2" },
+          { name: "Garlic", quantity: "1" },
+          { name: "Carrots", quantity: "5" },
+          { name: "Celery", quantity: "2" },
+          { name: "Potatoes", quantity: "10" },
+          { name: "Parsnips", quantity: "3" }
+      ];
 
-var shoppingList2 = [
-  {
-    name: "testing",
-    quantity: "10"
-  }
-];
+    angular.module('ShoppingListCheckOff', [])
+    .controller('ToBuyShoppingController', ToBuyShoppingController)
+    .controller('AlreadyBoughtController', AlreadyBoughtController)
+    .service('ShoppingListCheckOff', ShoppingListCheckOffService);
 
-  angular.module("ShoppingListCheckOffServiceApp", [])
-  .controller("ToBuyController", ToBuyController)
-  .controller("AlreadyBoughtController", AlreadyBoughtController);
+    ToBuyShoppingController.$inject = ['ShoppingListCheckOff'];
+    function ToBuyShoppingController(ShoppingListCheckOff){
+        var tobuy = this;
+        tobuy.items= ShoppingListCheckOff.getItemsToBuy();
+        tobuy.bought = function (itemIndex) {
+          ShoppingListCheckOff.bought(itemIndex);
+        }   
+    } 
 
-  ToBuyController.$inject = ["$scope"];
-
-  function ToBuyController() {
-    var tobuy = this
-    tobuy.shoppingList = shoppingList
+    AlreadyBoughtController.$inject = ['ShoppingListCheckOff'];
+    function AlreadyBoughtController(ShoppingListCheckOff){
+      var alreadybought=this;
+      alreadybought.items=ShoppingListCheckOff.getItemsBought();
     
-    // console.log("shoppinglist: ", shoppingList)
-    // console.log("shoppingList[0].name: ", shoppingList[0].name)
-    // console.log("shoppingList[0].quantity: ", shoppingList[0].quantity)
-    // $scope.string = "";
-    // $scope.message = "";
-    // $scope.status = "";
-    // $scope.col = "";
-    // NEED A FUNCTION TO:
-    // remove the item from the "toBuy" list on click
-    // Must happen on ng-click
+    }
+
+    function ShoppingListCheckOffService() {
+      var service = this;
+      var itemsBuy = shoppingList; 
+      var itemsBought=[];          
+      
+      service.bought = function (itemIndex) {
+        var item = {
+        
+        name: itemsBuy[itemIndex].name,
+        quantity: itemsBuy[itemIndex].quantity
+      };
+
+        itemsBought.push(item); 
+        itemsBuy.splice(itemIndex, 1); 
+      };
+
+     
+    service.getItemsToBuy = function () {
+      return itemsBuy;
+    };
+
+    service.getItemsBought = function () {
+      return itemsBought;
+    };
   }
-  AlreadyBoughtController.$inject = ["$scope"];
-  function AlreadyBoughtController($scope) {
-    var alreadybought = this;
-    alreadybought.shoppingList = shoppingList2
-    console.log(alreadybought.shoppingList)
 
-    // $scope.string = "";
-    // $scope.message = "";
-    // $scope.status = "";
-    // $scope.col = "";
-
-    // NEED A FUNCTION TO:
-    // add the item removed from the tobuy list to the alreadybought list
-    // Must happen on ng-click
-
-  }
-
-}());
+})();

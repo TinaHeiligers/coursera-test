d2dbbde291b853cbfb211e2b084e5d5878c7d7d2
TinaHeiligers <tina.heiligers@gmail.com>
2016-10-21 12:24:37 -0700

    redid

---
+++ module4_solution/src/menu-app/controllers/categories.controller.js
@@ -0,0 +1,9 @@
+(function() {
+	'use strict';
+
+	angular.module('MenuApp')
+	.controller('CategoriesController', ['categories', function(categories) {
+		this.categories = categories;
+	}]);
+
+})();
--- module4_solution/src/modules/data.module.js
+++
@@ -1,7 +0,0 @@
-(function() {
-  'use strict';
-
-  angular.module('data', [])
-  .constant('API_URL', 'https://davids-restaurant.herokuapp.com');
-
-})();
